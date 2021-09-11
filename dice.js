5e6d6ccd8d19b2a4abb2194435e8361ad340bbd5
ArT-apurv <89764232+ArT-apurv@users.noreply.github.com>
2021-09-11 22:04:25 +0530

    New Game xD

--- dice.js
+++ dice.js
@@ -1,26 +1,36 @@
-var a = Math.random();
-a = (a*6)+1;
-a = Math.floor(a);
+function myFunction() {
+    var a = Math.random();
+    a = (a * 6) + 1;
+    a = Math.floor(a);
 
-var image1 = "https://art-apurv.github.io/Dicee/images/dice" + a + ".png";
-document.querySelectorAll("img")[0].setAttribute("src",image1);
+    var image1 = "https://art-apurv.github.io/Dicee/images/dice" + a + ".png";
+    document.querySelectorAll("img")[0].setAttribute("src", image1);
 
-var b = Math.random();
-b = (b*6)+1;
-b = Math.floor(b);
+    var b = Math.random();
+    b = (b * 6) + 1;
+    b = Math.floor(b);
 
-var image2 = "https://art-apurv.github.io/Dicee/images/dice" + b + ".png";
-document.querySelectorAll("img")[1].setAttribute("src",image2);
+    var image2 = "https://art-apurv.github.io/Dicee/images/dice" + b + ".png";
+    document.querySelectorAll("img")[1].setAttribute("src", image2);
 
-if(a === b)
-{
-    document.querySelector("h1").innerHTML = ("No Winner");
+    if (a === b) {
+        document.querySelector("h1").innerHTML = ("No Winner");
+    } else if (a > b) {
+        document.querySelector("h1").innerHTML = ("First player won");
+    } else if (a < b) {
+        document.querySelector("h1").innerHTML = ("Second player won");
+    }
 }
-else if(a > b)
-{
-    document.querySelector("h1").innerHTML = ("First player won");
+
+window.onload = function() {
+    var reloading = sessionStorage.getItem("reloading");
+    if (reloading) {
+        sessionStorage.removeItem("reloading");
+        myFunction();
+    }
 }
-else if(a < b)
-{
-    document.querySelector("h1").innerHTML = ("Second player won");
+
+function reloadP() {
+    sessionStorage.setItem("reloading", "true");
+    document.location.reload();
 }
